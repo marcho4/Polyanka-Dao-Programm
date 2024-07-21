3e70c89e2959ce73620bf646a3ab88cb8a989580
marcho <madergilev@edu.hse.ru>
2024-07-22 01:05:09 +0300

    added_upd_func

--- programs/my-program/src/lib.rs
+++ programs/my-program/src/lib.rs
@@ -2,18 +2,16 @@ use anchor_lang::prelude::*;
 use solana_program::pubkey;
 use anchor_lang::solana_program::program::invoke;
 use anchor_lang::solana_program::system_instruction;
-
-
 extern crate chrono;
 use chrono::prelude::*;
 
-declare_id!("9WRa3Dpz2Di4awYBmdvUwh9gawrQLi29exR8RctkEMhj");
+declare_id!("2Qo3f8jvQZ8mJqLdm95DPBsC19EKKwCi3ohjGZkBrTC1");
 
 // Wallet, that will receive payments
 const HARDCODED_PUBKEY: Pubkey = pubkey!("6y1Fh4cFHrzASYsQZmxsXZSfxJL1ecSmsDJAn2kw1Fht");
-
 // Price of the subscription
 const RENEWAL: u64 = 150000000; 
+
 
 #[program]
 pub mod my_program {
@@ -47,17 +45,46 @@ pub mod my_program {
             err
         })?;
 
-        
-        if storage.end_date != 0 {
-            storage.end_date += 2629743;
-        } else {
-            storage.end_date = Clock::get()?.unix_timestamp + 2629743;
-        }
+        storage.end_date = Clock::get()?.unix_timestamp + 2592000;
         naive = NaiveDateTime::from_timestamp(storage.end_date, 0);
         datetime = DateTime::from_utc(naive, Utc);
         newdate = datetime.format("%Y-%m-%d %H:%M:%S");
 
-        msg!("Your subscription is active before {}", newdate);
+        msg!("End date: {}", newdate);
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, username: String) -> Result<()> {
+        let pda_account = &mut ctx.accounts.storage;
+
+        let transfer_instruction = system_instruction::transfer(
+            &ctx.accounts.payer.key(),
+            &HARDCODED_PUBKEY.key(),
+            RENEWAL,
+        );
+
+        invoke(
+            &transfer_instruction,
+            &[
+                ctx.accounts.payer.to_account_info(),
+                ctx.accounts.receiver.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+        )
+        .map_err(|err| {
+            msg!("CPI failed: {:?}", err);
+            err
+        })?;
+
+        if pda_account.end_date == 0 {
+            msg!("Account is not initialized!");
+        } else {
+            pda_account.end_date += 2592000;
+            let naive1 = NaiveDateTime::from_timestamp(pda_account.end_date, 0);
+            let datetime1: DateTime<Utc> = DateTime::from_utc(naive1, Utc);
+            let newdate1 = datetime1.format("%Y-%m-%d %H:%M:%S");
+            msg!("New end date: {}", newdate1);
+        }
         Ok(())
     }
 }
@@ -80,6 +107,21 @@ pub struct Initialize<'info> {
     #[account(mut, address = HARDCODED_PUBKEY)]
     pub receiver: UncheckedAccount<'info>,
    
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(username: String)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(mut, seeds = [b"polyanka_dao_program", username.as_bytes()], bump)]
+    pub storage: Account<'info, UserInfo>,
+
+    /// CHECK: Here you should put public key of community boss
+    #[account(mut, address = HARDCODED_PUBKEY)]
+    pub receiver: UncheckedAccount<'info>,
     pub system_program: Program<'info, System>,
 }
 
--- tests/my-program.ts
+++ tests/my-program.ts
@@ -1,22 +1,122 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { MyProgram } from "../target/types/my_program";
-import { Keypair, PublicKey} from "@solana/web3.js"
-import bs58 from 'bs58';
+import { Keypair, Connection, PublicKey, Transaction, SystemProgram, sendAndConfirmTransaction, TransactionInstruction} from "@solana/web3.js";
+import {
+  deserialize,
+  serialize,
+  field,
+} from "@dao-xyz/borsh";
+import * as bs from "bs58"
+import { use } from "chai";
+
+
+// class IxnData
+// {
+//   @field({type: 'u64'})
+//   discriminator: bigint
+
+//   @field({type: 'string'})
+//   username: string
+
+//   constructor(data: IxnData)
+//   {
+//       Object.assign(this, data)
+//   }
+// }
+
+// // Program and Wallet Setup
+// const programId = new PublicKey("9WRa3Dpz2Di4awYBmdvUwh9gawrQLi29exR8RctkEMhj");
+// const con = new Connection("http://localhost:8899");
+// const payer = Keypair.fromSecretKey(new Uint8Array([60,47,24,41,124,38,29,174,148,9,75,149,213,111,119,71,15,176,180,5,2,166,29,48,17,117,165,158,65,113,58,87,174,62,177,215,55,175,64,51,59,214,219,41,188,114,34,240,162,219,28,151,218,1,55,207,57,167,11,32,9,226,96,165]));
+
+// // Tx Instruction data to be encoded
+const username = "egor.sol"
+// const discriminator = 17121445590508351407n
+
+// // Serialization Data
+// const value = new IxnData({discriminator: discriminator, username: username});
+// const serialized = serialize(value);
+// const data = bs.default.encode(serialized);
+
+// // Getting PDA
+// const [PDA] = PublicKey.findProgramAddressSync(
+//   [
+//     Buffer.from("polyanka_dao_program"),
+//     Buffer.from(username)
+//   ],
+//   programId
+// )
+// console.log(data)
+
+// const instruction = new TransactionInstruction({
+//   keys: [
+//     {
+//       pubkey: payer.publicKey,
+//       isSigner: true,
+//       isWritable: true,
+//     },
+//     {
+//       pubkey: PDA,
+//       isSigner: false,
+//       isWritable: false
+//     },
+//     {
+//       pubkey: new PublicKey("6y1Fh4cFHrzASYsQZmxsXZSfxJL1ecSmsDJAn2kw1Fht"),
+//       isSigner: false,
+//       isWritable: true
+//     },
+//     {
+//       pubkey: SystemProgram.programId,
+//       isSigner: false,
+//       isWritable: false
+//     }
+//   ],
+//   data: data,
+//   programId
+// })
+
+// let transaction = new Transaction().add(instruction)
+
+// const signature = sendAndConfirmTransaction(
+//   con,
+//   transaction,
+//   [payer]
+// )
+
+// console.log(signature)
 
 describe("my-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
   const provider =  anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.MyProgram as Program<MyProgram>;
-
-  let username = "huy";
   const wallet = provider.wallet as anchor.Wallet;
 
-  it("Is initialized!", async () => {
+  // it("Is initialized!", async () => {
+  //   const [PDA] = await PublicKey.findProgramAddressSync(
+  //     [
+  //       Buffer.from("polyanka_dao_program"),
+  //       Buffer.from(username)
+  //     ],
+  //     program.programId
+  //   )
+  //   const tx = await program.methods.initialize(username)
+  //   .accounts({
+  //     storage: PDA, 
+  //     payer: wallet.publicKey,
+  //     receiver: "6y1Fh4cFHrzASYsQZmxsXZSfxJL1ecSmsDJAn2kw1Fht"
+  //   })
+  //   .rpc();
+
+  //   console.log("Your transaction signature", tx);
+
+  //   const data = await program.account.userInfo.fetch(PDA);
+  //   console.log(data.endDate.toString());
+  //   console.log(data);
+  // });
+
+  it("Updated", async () => {
     const [PDA] = await PublicKey.findProgramAddressSync(
       [
         Buffer.from("polyanka_dao_program"),
@@ -24,7 +124,7 @@ describe("my-program", () => {
       ],
       program.programId
     )
-    const tx = await program.methods.initialize(username)
+    const tx = await program.methods.update(username)
     .accounts({
       storage: PDA, 
       payer: wallet.publicKey,
@@ -35,7 +135,8 @@ describe("my-program", () => {
     console.log("Your transaction signature", tx);
 
     const data = await program.account.userInfo.fetch(PDA);
-    console.log(data.endDate.toString());
-    console.log(data);
+    const unixtime = data.endDate.toNumber();
+    let d = new Date(unixtime * 1000);
+    console.log(d.toDateString());
   });
 });
